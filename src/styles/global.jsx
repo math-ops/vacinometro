4ae7fd88ae33dbabecf6ed0e418f97a73eb7e6f0
Matheus Oliveira <matheusops.pc@gmail.com>
2021-09-29 13:01:01 -0400

    Update global.jsx
    Atualização nas resoluções do dashboard.

--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -55,6 +55,11 @@ export default createGlobalStyle`
         margin-left: -200px;
         margin-top: 30px;
     }
+
+    .selection {        
+        margin-left: -200px;
+        margin-top: 70px;  
+    }
 }
 
 ///////////////////DESKTOP RESULÇAO 1360p///////////////////
@@ -104,6 +109,65 @@ export default createGlobalStyle`
         margin-left: -180px;
         margin-top: -15px;
     }
+
+    .selection {        
+        margin-left: -170px;
+        margin-top: 70px;  
+    }
+
+}
+
+@media only screen and (min-width: 1600px){
+    html{
+    font-family: "Oswald", sans-serif;
+    font-size: 86.25%;
+    }
+
+    body {        
+    zoom: 80%;
+    overflow-x: hidden;
+    overflow-y: hidden; 
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    img {
+        margin-right: 40px;
+    }
+
+    .backgroundcover {
+        background: #eee;
+        height: 140vh;
+    }
+
+    .bgpng {
+        position: absolute;
+        width: 110%;
+        height: 100%;
+        margin-left: -150px;        
+    }   
+
+    .svg {
+        position: absolute;
+        margin-top: -100px;
+        margin-left: 920px;
+        width: 100px;
+        height: 100px;
+    }    
+
+    .dashboard {
+        width: 50px;
+        padding: 1px;
+        margin-left: -80px;
+        margin-top: -120px;
+    }
+
+    .selection {        
+        margin-left: -70px;
+        margin-top: -100px;  
+    }
 }
 
 ///////////////////DESKTOP RESULÇAO 1720p///////////////////
@@ -150,8 +214,13 @@ export default createGlobalStyle`
     .dashboard {
         width: 50px;
         padding: 1px;
-        margin-left: -150px;
-        margin-top: -500px;
+        margin-left: -120px;
+        margin-top: -390px;
+    }
+
+    .selection {        
+        margin-left: -100px;
+        margin-top: -270px;  
     }
 }
 
